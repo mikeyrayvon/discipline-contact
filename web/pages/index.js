fd6bd3a1a47e2656d7cb6a698beb63ca7618aad9
mikeyrayvon <mikey@interglobal.vision>
2021-01-12 23:30:53 +0100

    add footer to index. adjust layout

--- web/pages/index.js
+++ web/pages/index.js
@@ -6,12 +6,12 @@ import Container from '../components/Container'
 import sanity from '../lib/sanity'
 
 const query = `*[_type == "page"] {
+  about,
   "projects": projects[]->
 }
 `;
 
 const Landing = ({ docs }) => {
-  console.log(docs)
   return (
     <div className='flex flex-col min-h-screen'>
       <Head>
@@ -19,18 +19,19 @@ const Landing = ({ docs }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='mt-40 mb-24'>
-        <header className='mb-24'>
+      <header className='mt-40 mb-24 flex-grow'>
+        <Container>
+          <img src='discipline-logo.svg' className='w-32 md:w-48 lg:w-64' />
+        </Container>
+      </header>
+
+      <main>
+        <section className='mb-24'>
           <Container>
-            <img src='discipline-logo.svg' className='w-32 md:w-48 lg:w-64' />
-          </Container>
-        </header>
-        <section>
-          <Container>
-            <ul className='flex flex-wrap -mx-12'>
+            <ul className='flex flex-wrap -mx-8'>
               {docs[0].projects.map(project => {
                 return (
-                  <li className='px-12 mb-12' key={`project_${project._id}`}>
+                  <li className='px-8 mb-12' key={`project_${project._id}`}>
                     <Link href={project.link}>
                       <a>
                         {project.title}
@@ -43,6 +44,21 @@ const Landing = ({ docs }) => {
           </Container>
         </section>
       </main>
+
+      <footer className='mb-24'>
+        <Container>
+          <div className='flex flex-wrap lg:flex-nowrap -mx-8'>
+            <div className='px-8'>
+              {docs[0].about}
+            </div>
+            <div className='px-8'>
+              <Link href='mailto:contact@discipline.contact'>
+                <a>contact@discipline.contact</a>
+              </Link>
+            </div>
+          </div>
+        </Container>
+      </footer>
     </div>
   )
 }
