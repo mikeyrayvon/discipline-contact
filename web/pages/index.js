e860fffab3039f125e4ff87b4e0823e86235ffc3
mikeyrayvon <mikey@interglobal.vision>
2021-01-12 22:59:39 +0100

    add projects list

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+import Container from '../components/Container'
+
 import sanity from '../lib/sanity'
 
 const query = `*[_type == "page"] {
-  _id,
-  title
+  "projects": projects[]->
 }
 `;
 
@@ -18,8 +19,29 @@ const Landing = ({ docs }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <img src='discipline-logo.svg' className='w-60' />
+      <main className='mt-40 mb-24'>
+        <header className='mb-24'>
+          <Container>
+            <img src='discipline-logo.svg' className='w-32 md:w-48 lg:w-64' />
+          </Container>
+        </header>
+        <section>
+          <Container>
+            <ul className='flex flex-wrap -mx-12'>
+              {docs[0].projects.map(project => {
+                return (
+                  <li className='px-12 mb-12' key={`project_${project._id}`}>
+                    <Link href={project.link}>
+                      <a>
+                        {project.title}
+                      </a>
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </Container>
+        </section>
       </main>
     </div>
   )
